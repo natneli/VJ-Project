92883a8b8afc81c8912b7e909909442be231ded2
natneli <natanhaileselassie@gmail.com>
2024-01-07 23:18:04 -0700

    PDF started rendering but wrongly

--- src/Componenets/ProductTypesCIJ.js
+++ src/Componenets/ProductTypesCIJ.js
@@ -64,7 +64,7 @@
 //       "Scratchpads",
 //       "Maintenance Items",
 //     ],
-//     "Simplicity": [
+//     Simplicity: [
 //       "Parts Identification",
 //       "Troublshooting",
 //       "Disassembly and Assembly",
@@ -79,12 +79,30 @@
 //     ],
 //   };
 
-//   const Sections = [
-//     "Electronic Compartment",
-//     "Ink Compartment",
-//     "Printhead",
-//     "Body",
-//   ];
+//   const Sections = {
+//     "Parts Identification": [
+//       "Electronic Compartment",
+//       "Ink Compartment",
+//       "Printhead",
+//       "Body",
+//     ],
+//     Troublshooting: ["Problem1", "Problem2", "Problem3", "Problem5"],
+//     "Disassembly and Assembly": [
+//       "Printhead",
+//       "Valve Deck",
+//       "Ink Core",
+//       "Umbilical",
+//       "MainBoard",
+//     ],
+//     "Service Manual": ["Service Manual1", "Service Manual2"],
+//     "Fluid Diagrams": ["Diagram1", "Diagram2"],
+//     "Electrical Schematics": ["Schematic1", "Schematics2"],
+//     Specification: ["Spec1", "Spec2"],
+//     "Service Bulletins": ["Bul1", "Bul2"],
+//     "Service Software": ["SW1", "SW2", "SW3"],
+//     Scratchpads: ["SPS1", "SPS2", "SPS3"],
+//     "Maintenance Items": ["M1", "M2", "M3"],
+//   };
 
 //   const handleSubmodelClick = (subModel) => {
 //     setOpenSubmodel((prev) => (prev === subModel ? null : subModel));
@@ -128,8 +146,8 @@
 //                   {subModel}
 //                   {openContent === subModel && (
 //                     <div className="content-dropdown">
-//                       {contents[model].map((content) => (
-//                         <div key={content}>
+//                       {contents[model].map((content, index) => (
+//                         <div key={index}>
 //                           <span
 //                             className="plus-sign-content"
 //                             onClick={() => handleSectionClick(content)}
@@ -139,10 +157,8 @@
 //                           {content}
 //                           {openSection === content && (
 //                             <div className="section-dropdown">
-//                               {Sections.map((Section) => (
-//                                 <div key={Section}>
-//                                   {Section}
-//                                 </div>
+//                               {Sections[content].map((section, index) => (
+//                                 <div key={index}>{section}</div>
 //                               ))}
 //                             </div>
 //                           )}
@@ -163,9 +179,15 @@
 // export default ProductTypesCIJ;
 
 
-import React, { useState } from "react";
+
+
+
+
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./ProductTypesCIJ.css";
+
+
 
 const ProductTypesCIJ = () => {
   const { selectedButton } = useParams();
@@ -205,7 +227,7 @@ const ProductTypesCIJ = () => {
   const contents = {
     "1000 Line": [
       "Parts Identification",
-      "Troublshooting",
+      "Troubleshooting",
       "Disassembly and Assembly",
       "Service Manual",
       "Fluid Diagrams",
@@ -218,7 +240,7 @@ const ProductTypesCIJ = () => {
     ],
     "Pigmented Ink(1000 Line)": [
       "Parts Identification",
-      "Troublshooting",
+      "Troubleshooting",
       "Disassembly and Assembly",
       "Service Manual",
       "Fluid Diagrams",
@@ -231,7 +253,7 @@ const ProductTypesCIJ = () => {
     ],
     Simplicity: [
       "Parts Identification",
-      "Troublshooting",
+      "Troubleshooting",
       "Disassembly and Assembly",
       "Service Manual",
       "Fluid Diagrams",
@@ -251,7 +273,7 @@ const ProductTypesCIJ = () => {
       "Printhead",
       "Body",
     ],
-    Troublshooting: ["Problem1", "Problem2", "Problem3", "Problem5"],
+    Troubleshooting: ["Problem1", "Problem2", "Problem3", "Problem5"],
     "Disassembly and Assembly": [
       "Printhead",
       "Valve Deck",
@@ -270,20 +292,36 @@ const ProductTypesCIJ = () => {
   };
 
   const handleSubmodelClick = (subModel) => {
+    console.log("handleSubmodelClick", subModel);
     setOpenSubmodel((prev) => (prev === subModel ? null : subModel));
     setOpenContent(null);
     setOpenSection(null);
   };
 
   const handleContentClick = (content) => {
+    console.log("handleContentClick", content);
     setOpenContent((prev) => (prev === content ? null : content));
     setOpenSection(null);
   };
 
   const handleSectionClick = (section) => {
+    console.log("handleSectionClick", section);
     setOpenSection((prev) => (prev === section ? null : section));
   };
 
+  const shouldRenderPDFViewer =
+    openSubmodel === "1000 Line" &&
+    openContent === "1210-1510" &&
+    openSection === "Parts Identification";
+
+      console.log("shouldRenderPDFViewer", shouldRenderPDFViewer);
+
+        useEffect(() => {
+          console.log("openSubmodel", openSubmodel);
+          console.log("openContent", openContent);
+          console.log("openSection", openSection);
+          console.log("shouldRenderPDFViewer", shouldRenderPDFViewer);
+        }, [openSubmodel, openContent, openSection]);
   return (
     <div className="product-types-cij-container">
       <h2>{selectedButton}</h2>
@@ -337,8 +375,31 @@ const ProductTypesCIJ = () => {
           )}
         </div>
       ))}
+      {/* Log values for debugging */}
+      {console.log("openSubmodel", openSubmodel)}
+      {console.log("openContent", openContent)}
+      {console.log("openSection", openSection)}
+      {console.log(
+        "Condition",
+        openSubmodel === "1000 Line" &&
+          openContent === "1210-1510" &&
+          openSection === "Parts Identification"
+      )}
+
+      {openSubmodel === "1000 Line" &&
+        openContent === "1210-1510" &&
+        openSection === "Parts Identification" && (
+          <div className="pdf-viewer-container">
+            <iframe
+              alt="Image Viewer" 
+              src={`${process.env.PUBLIC_URL}/PDF Files/Basics.pdf`}
+              width="300" height="200" />
+           
+          </div>
+        )}
     </div>
   );
+
 };
 
 export default ProductTypesCIJ;
@@ -346,199 +407,3 @@ export default ProductTypesCIJ;
 
 
 
-
-
-// import React, { useState } from "react";
-// import { useParams } from "react-router-dom";
-//  import "@react-pdf-viewer/react-pdf/dist/esm/index.css";
-// import { Worker, Viewer } from "@react-pdf-viewer/core";
-// import "./ProductTypesCIJ.css";
-
-// const ProductTypesCIJ = () => {
-//   const { selectedButton } = useParams();
-//   const [openSubmodel, setOpenSubmodel] = useState(null);
-//   const [openContent, setOpenContent] = useState(null);
-//   const [openSection, setOpenSection] = useState(null);
-//   const [selectedPDF, setSelectedPDF] = useState(null); // State to track the selected PDF
-
-//   const submodels = {
-//     "1000 Line": [
-//       "1210-1510",
-//       "1220-1520",
-//       "1510E",
-//       "1520E",
-//       "1550",
-//       "1610",
-//       "1610DH",
-//       "1610E",
-//       "1610E DH",
-//       "1620E",
-//       "1650",
-//       "1650-1650 UHS",
-//     ],
-//     "Pigmented Ink(1000 Line)": ["1710"],
-//     Simplicity: [
-//       "1280",
-//       "1580",
-//       "1860",
-//       "1880",
-//       "1580C",
-//       "1580+",
-//       "1880+",
-//       "1880 UHS",
-//       "1880 HR",
-//     ],
-//   };
-
-//   const contents = {
-//     "1000 Line": [
-//       "Parts Identification",
-//       "Troubleshooting",
-//       "Disassembly and Assembly",
-//       "Service Manual",
-//       "Fluid Diagrams",
-//       "Electrical Schematics",
-//       "Specification",
-//       "Service Bulletins",
-//       "Service Software",
-//       "Scratchpads",
-//       "Maintenance Items",
-//     ],
-//     "Pigmented Ink(1000 Line)": [
-//       "Parts Identification",
-//       "Troubleshooting",
-//       "Disassembly and Assembly",
-//       "Service Manual",
-//       "Fluid Diagrams",
-//       "Electrical Schematics",
-//       "Specification",
-//       "Service Bulletins",
-//       "Service Software",
-//       "Scratchpads",
-//       "Maintenance Items",
-//     ],
-//     Simplicity: [
-//       "Parts Identification",
-//       "Troubleshooting",
-//       "Disassembly and Assembly",
-//       "Service Manual",
-//       "Fluid Diagrams",
-//       "Electrical Schematics",
-//       "Specification",
-//       "Service Bulletins",
-//       "Service Software",
-//       "Scratchpads",
-//       "Maintenance Items",
-//     ],
-//   };
-
-//   const Sections = {
-//     "Parts Identification": [
-//       "Electronic Compartment",
-//       "Ink Compartment",
-//       "Printhead",
-//       "Body",
-//     ],
-//     Troubleshooting: ["Problem1", "Problem2", "Problem3", "Problem5"],
-//     "Disassembly and Assembly": [
-//       "Printhead",
-//       "Valve Deck",
-//       "Ink Core",
-//       "Umbilical",
-//       "MainBoard",
-//     ],
-//     "Service Manual": ["Service Manual1", "Service Manual2"],
-//     "Fluid Diagrams": ["Diagram1", "Diagram2"],
-//     "Electrical Schematics": ["Schematic1", "Schematics2"],
-//     Specification: ["Spec1", "Spec2"],
-//     "Service Bulletins": ["Bul1", "Bul2"],
-//     "Service Software": ["SW1", "SW2", "SW3"],
-//     Scratchpads: ["SPS1", "SPS2", "SPS3"],
-//     "Maintenance Items": ["M1", "M2", "M3"],
-//   };
-
-//   const handleSubmodelClick = (subModel) => {
-//     setOpenSubmodel((prev) => (prev === subModel ? null : subModel));
-//     setOpenContent(null);
-//     setOpenSection(null);
-//     setSelectedPDF(null); // Close PDF viewer when selecting a new submodel
-//   };
-
-//   const handleContentClick = (content) => {
-//     setOpenContent((prev) => (prev === content ? null : content));
-//     setOpenSection(null);
-//     setSelectedPDF(null); // Close PDF viewer when selecting a new content
-//   };
-
-//   const handleSectionClick = (section) => {
-//     setOpenSection((prev) => (prev === section ? null : section));
-//     setSelectedPDF(section); // Set the selected PDF when clicking a section
-//   };
-
-//   return (
-//     <div className="product-types-cij-container">
-//       <h2>{selectedButton}</h2>
-//       {Object.keys(submodels).map((model) => (
-//         <div key={model} className="model-container">
-//           <div className="model-content">
-//             <span
-//               className={`plus-sign ${openSubmodel === model ? "open" : ""}`}
-//               onClick={() => handleSubmodelClick(model)}
-//             >
-//               {openSubmodel === model ? "-" : "+"}
-//             </span>
-//             <span className="model-text">{model}</span>
-//           </div>
-//           {openSubmodel === model && (
-//             <div className="submodel-dropdown">
-//               {submodels[model].map((subModel) => (
-//                 <div key={subModel}>
-//                   <span
-//                     className="plus-sign-submodel"
-//                     onClick={() => handleContentClick(subModel)}
-//                   >
-//                     +
-//                   </span>
-//                   {subModel}
-//                   {openContent === subModel && (
-//                     <div className="content-dropdown">
-//                       {contents[model].map((content) => (
-//                         <div key={content}>
-//                           <span
-//                             className="plus-sign-content"
-//                             onClick={() => handleSectionClick(content)}
-//                           >
-//                             +
-//                           </span>
-//                           {content}
-//                           {openSection === content && (
-//                             <div className="section-dropdown">
-//                               {Sections[content].map((section) => (
-//                                 <div key={section}>{section}</div>
-//                               ))}
-//                             </div>
-//                           )}
-//                         </div>
-//                       ))}
-//                     </div>
-//                   )}
-//                 </div>
-//               ))}
-//             </div>
-//           )}
-//         </div>
-//       ))}
-//       <div className="pdf-viewer-container">
-//         {selectedPDF && (
-//           <Worker
-//             workerUrl={`https://unpkg.com/pdfjs-dist@${"4.0.379"}/build/pdf.worker.min.js`}
-//           >
-//             <Viewer file={`../../src/PDF Files/Basics.pdf`} />
-//           </Worker>
-//         )}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ProductTypesCIJ;
