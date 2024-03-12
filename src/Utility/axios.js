68e0b2668d80fdafc10aa2fa1d8722e607cce540
natneli <natanhaileselassie@gmail.com>
2024-03-12 01:03:48 -0600

    changed base url

--- src/Utility/axios.js
+++ src/Utility/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-let BASE_URL = "http://localhost:5004";
+// let BASE_URL = "http://localhost:5004";
+let BASE_URL = "https://natneli.github.io/VJ-Project/";
 
 const axiosInstance = axios.create({
   withCredentials: true,
